df45e2925f5fb9b1798950960d9736882c500c41
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-13 17:50:33 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"How ...... when you ...... the film?",
+        options:["did you feel / saw", "did you felt / see"],
+        answer: 0
+    },
+    {
+        q:"Hans ...... the initials on the cave wall.",
+        options:["didn't found", "didn't find"],
+        answer: 1
+    },
+    {
+        q:"Where ...... ?",
+        options:["did Captain Nemo travel", "did Captain Nemo traveled"],
+        answer: 0
+    },
+    {
+        q:"...... the cave? - No, he ...... afraid." ,
+        options:["Did he explored / were", "Did he explore / was"],
+        answer: 1
+    },
+    {
+        q:"When ...... ? - An hour ago. He ...... Ann with him.",
+        options:["did he arrive / brought", "did he arrived / bring"],
+        answer: 0
+    }
+
+
+
+]
